5476af859790f5b9d44325bb6d333bd3f15e8a6b
lodio-999 <132812071+lodio-999@users.noreply.github.com>
2023-05-07 09:36:00 -0700

    Update xmr-pool.bat

--- xmr-pool.bat
+++ xmr-pool.bat
@@ -1,2 +1,1 @@
-xmrig.exe -B --cuda -o xmr-eu2.nanopool.org:14433 -u "44dC3rKKRNy5M3jdtEJ7MuTGWqvkyHoWuhFh87wtduHVMXSeXX3Z9qxEQfpYC1vMfKK62Wrmi1tgo1cLaSkZ15YpPN3eNNy.hero/lodioinfo@gmail.com" --tls --coin monero
-pause
+echo hi
